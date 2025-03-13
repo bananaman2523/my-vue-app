2a120d660d37fade490d7678748a3ed4cf2d9091
Pattarapol Tae <pattarapl28.29@gmail.com>
2025-03-13 16:33:55 +0700

    t

--- src/services/directus.js
+++ src/services/directus.js
@@ -16,5 +16,5 @@ class LocalStorage {
 
 const storage = new LocalStorage();
 
-export const directus = createDirectus('http://192.168.1.51:8055').with(authentication('json', { storage })).with(rest());
+export const directus = createDirectus('http://localhost:8055').with(authentication('json', { storage })).with(rest());
 
